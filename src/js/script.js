d2b5eedda74c8ebf9401701b4c44731cca20fc14
Alyona <alenka.inform@gmail.com>
2022-12-24 08:59:56 +0300

    Add folder modules

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,10 @@
-import {initAccordion, mediaQuery} from "./accordion-phone.js";
-import {countsLengthComment} from "./comment-length.js";
-import {initAddressHints} from "./dadata.js";
-// import {opensBurgerMenu} from "./open-burger.js";
-import {scrollsUp} from "./scroll-up.js";
-import {transfersPlaceholder} from "./transfer-placeholder.js";
-import {initMap} from "./yandex-map.js";
+import {initAccordion, mediaQuery} from "./modules/accordion-phone.js";
+import {countsLengthComment} from "./modules/comment-length.js";
+import {initAddressHints} from "./modules/dadata.js";
+// import {opensBurgerMenu} from "./modules/open-burger.js";
+import {scrollsUp} from "./modules/scroll-up.js";
+import {transfersPlaceholder} from "./modules/transfer-placeholder.js";
+import {initMap} from "./modules/yandex-map.js";
 
 // opensBurgerMenu();
 window.addEventListener('DOMContentLoaded', () => {
