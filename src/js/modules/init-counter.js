e0eedaa0526c6e6ca12388b0843bc4f85bc64394
Alyona <alenka.inform@gmail.com>
2022-12-24 13:36:16 +0300

    Revive the counter

--- src/js/modules/init-counter.js
+++ src/js/modules/init-counter.js
@@ -2,37 +2,53 @@ const initCounter = () => {
   const counterWrappers = document.querySelectorAll('[data-counters');
 
   if (counterWrappers) {
+    // let totalCost = 0;
+
     counterWrappers.forEach(counter => {
       const plus = counter.querySelector('.card__result-button--plus');
       const minus = counter.querySelector('.card__result-button--minus');
-      let value = counter.querySelector('.card__result-quantity-value').value;
-      let valueInt = parseInt(value);
+      let input = counter.querySelector('.card__result-quantity-value');
 
-      if (valueInt <= 1) {
-        valueInt = 1;
+      if (input.value <= 1) {
+        input.value = 1;
         minus.disabled = true;
       }
 
       plus.addEventListener('click', () => {
-        valueInt++;
-        if (valueInt > 1) {
+        input.value++
+        if (input.value > 1) {
           minus.disabled = false;
         }
-        value = valueInt;
-        console.log(value)
       })
 
       minus.addEventListener('click', () => {
-        valueInt--;
-        if (valueInt <= 1) {
-          valueInt = 1;
+        input.value--;
+        if (input.value <= 1) {
+          input.value = 1;
           minus.disabled = true;
         }
-        value = valueInt;
-        console.log(value)
       })
+
+      // totalCost += value * input.dataset.price;
+      // console.log(totalCost)
+
     })
   }
 }
 
 export {initCounter};
+
+
+// let prices = counter.querySelectorAll('[data-price]');
+// let pricesAll = counter.querySelectorAll('[data-price-all]');
+
+// prices.forEach(price => {
+//   let priceInt = parseInt(price.textContent.replace(/\s/g, ''));
+
+//   pricesAll.forEach(priceAll => {
+//     let priceIntAll = parseInt(priceAll.textContent.replace(/\s/g, ''));
+
+//     console.log(priceIntAll = (priceInt *= valueInt));
+//     priceAll.textContent = priceIntAll;
+//   })
+// })
