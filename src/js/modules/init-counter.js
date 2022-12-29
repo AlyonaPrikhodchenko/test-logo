438e912bdc25dadaa77db373768ffdcc0b25f15c
Alyona <alenka.inform@gmail.com>
2022-12-29 10:27:16 +0300

    Fix the code a little

--- src/js/modules/init-counter.js
+++ src/js/modules/init-counter.js
@@ -27,83 +27,109 @@ const initCounter = () => {
       let totalPriceItemOld = counter.querySelector('[data-text-old]');
       let input = counter.querySelector('.card__result-quantity-value');
 
-      if (counter) {
+      // Считаем общие суммы в карточке
+      const countsAmountsCard = () => {
+        input.dataset.totalPrice = input.value * input.dataset.price;
+        input.dataset.totalPriceOld = input.value * input.dataset.oldPrice;
+      }
 
+      // Считаем скидку по акциям и выводим на экран в строку 'Акции'
+      const countsStocks = () => {
+        priceStocks.dataset.priceStocks = Number(input.dataset.totalPriceOld) - Number(input.dataset.totalPrice);
+        priceStocks.textContent = formatNumber(priceStocks.dataset.priceStocks);
+      }
+
+      // Считаем сумму итоговую и выводим на экран в строку 'Итого'
+      const countsAllPrice = () => {
+        totalPriceAll.dataset.priceTotalAll = Number(totalPrice.dataset.total) + Number(priceDelivery.dataset.priceDelivery) - Number(priceDiscounts.dataset.priceDiscounts);
+        totalPriceAll.textContent = formatNumber(totalPriceAll.dataset.priceTotalAll);
+      }
+
+      // Считаем общую скидку и выводим на экран в строку 'Скидка'
+      const countsDiscounts = () => {
+        priceDiscounts.dataset.priceDiscounts = Number(priceStocks.dataset.priceStocks) + Number(pricePromo.dataset.pricePromo);
+        priceDiscounts.textContent = formatNumber(priceDiscounts.dataset.priceDiscounts);
+      }
+
+      // Показываем старую сумму (если старая сумма !== новой)
+      const displaysOldPriceText = () => totalPriceItemOld.textContent = formatNumber(input.dataset.totalPriceOld);
+
+      // Выводим текст с общей стоимостью в карточке
+      const displaysPriceText = () => totalPriceItem.textContent = formatNumber(input.dataset.totalPrice);
+
+      // Блокирует кнопку, если значение карточки = 1 и разблокирует, если больше 1
+      const disabledButton = () => {
         if (input.value <= 1) {
           input.value = 1;
           minus.disabled = true;
+        } else {
+          minus.disabled = false;
         }
+      }
 
-        totalPriceItem.textContent = formatNumber(input.dataset.totalPrice);
+      if (counter) {
+        disabledButton()
 
+        // При нажатии на +
         plus.addEventListener('click', () => {
           input.value++
-          input.dataset.totalPrice = input.value * input.dataset.price;
-          input.dataset.totalPriceOld = input.value * input.dataset.oldPrice;
+          countsAmountsCard();
 
           if (totalPriceItemOld) {
-            totalPriceItemOld.textContent = formatNumber(input.dataset.totalPriceOld);
-
-            priceStocks.dataset.priceStocks = Number(input.dataset.totalPriceOld) - Number(input.dataset.totalPrice);
-            priceStocks.textContent = formatNumber(priceStocks.dataset.priceStocks);
+            displaysOldPriceText()
+            countsStocks();
           }
 
-          totalPriceItem.textContent = formatNumber(input.dataset.totalPrice);
+          displaysPriceText()
 
+          // Считаем сумму при клике на + и выводим на экран в строку 'Товары'
           totalPrice.dataset.total = Number(totalPrice.dataset.total) + Number(input.dataset.oldPrice);
           totalPrice.textContent = formatNumber(totalPrice.dataset.total);
 
-          totalPriceAll.dataset.priceTotalAll = Number(totalPrice.dataset.total) + Number(priceDelivery.dataset.priceDelivery) - Number(priceDiscounts.dataset.priceDiscounts);
-          totalPriceAll.textContent = formatNumber(totalPriceAll.dataset.priceTotalAll);
+          countsDiscounts();
+          countsAllPrice();
 
-          priceDiscounts.dataset.priceDiscounts = Number(priceStocks.dataset.priceStocks) + Number(pricePromo.dataset.pricePromo);
-          priceDiscounts.textContent = formatNumber(priceDiscounts.dataset.priceDiscounts);
-
+          // Считаем общее количество товаров при клике на + и выводим на экран в заголовок списка товаров
           productsCounts.dataset.count = Number(productsCounts.dataset.count) + 1;
           productsCounts.textContent = productsCounts.dataset.count;
 
+          // Считаем общую сумму при клике на - и выводим на экран в заголовок списка товаров
           productsPrice.dataset.productsPrice = Number(productsPrice.dataset.productsPrice) + Number(input.dataset.oldPrice);
           productsPrice.textContent = formatNumber(productsPrice.dataset.productsPrice);
 
-          if (input.value > 1) {
-            minus.disabled = false;
-          }
+          disabledButton()
         })
 
+        // При нажатии на -
         minus.addEventListener('click', () => {
           input.value--;
-          input.dataset.totalPrice = input.value * input.dataset.price;
-          input.dataset.totalPriceOld = input.value * input.dataset.oldPrice;
+          countsAmountsCard();
 
           if (totalPriceItemOld) {
-            totalPriceItemOld.textContent = formatNumber(input.dataset.totalPriceOld);
-
-            priceStocks.dataset.priceStocks = Number(input.dataset.totalPriceOld) - Number(input.dataset.totalPrice);
-            priceStocks.textContent = formatNumber(priceStocks.dataset.priceStocks);
+            displaysOldPriceText()
+            countsStocks()
           }
 
-          totalPriceItem.textContent = formatNumber(input.dataset.totalPrice);
+          displaysPriceText()
 
+          // Считаем сумму при клике на - и выводим на экран в строку 'Товары'
           totalPrice.dataset.total = Number(totalPrice.dataset.total) - Number(input.dataset.oldPrice);
           totalPrice.textContent = formatNumber(totalPrice.dataset.total);
 
-          totalPriceAll.dataset.priceTotalAll = Number(totalPrice.dataset.total) + Number(priceDelivery.dataset.priceDelivery) - Number(priceDiscounts.dataset.priceDiscounts);
-          totalPriceAll.textContent = formatNumber(totalPriceAll.dataset.priceTotalAll);
+          countsDiscounts()
+          countsAllPrice();
 
-          priceDiscounts.dataset.priceDiscounts = Number(priceStocks.dataset.priceStocks) + Number(pricePromo.dataset.pricePromo);
-          priceDiscounts.textContent = formatNumber(priceDiscounts.dataset.priceDiscounts);
-
+          // Считаем общее количество товаров при клике на - и выводим на экран в заголовок списка товаров
           productsCounts.dataset.count = Number(productsCounts.dataset.count) - 1;
           productsCounts.textContent = productsCounts.dataset.count;
 
+          // Считаем общую сумму при клике на - и выводим на экран в заголовок списка товаров
           productsPrice.dataset.productsPrice = Number(productsPrice.dataset.productsPrice) - Number(input.dataset.oldPrice);
           productsPrice.textContent = formatNumber(productsPrice.dataset.productsPrice);
 
-          if (input.value <= 1) {
-            input.value = 1;
-            minus.disabled = true;
-          }
+          disabledButton()
         })
+
 
         totalPriceItem.textContent = formatNumber(input.dataset.totalPrice);
         totalCost += getItemPrice(input);
